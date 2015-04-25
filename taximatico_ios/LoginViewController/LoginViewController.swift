c1fe885edb0d9c847e2621126600ac119cf91615
Oscar Swanros <swanros@gmail.com>
2015-04-25 11:48:52 -0500

    create loginViewController

---
+++ taximatico_ios/LoginViewController/LoginViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.swift
+//  taximatico_ios
+//
+//  Created by Oscar Swanros on 4/25/15.
+//  Copyright (c) 2015 Taximatico. All rights reserved.
+//
+
+import Cocoa
+
+class LoginViewController: UIViewController {
+
+}
