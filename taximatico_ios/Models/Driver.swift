ede2d36b6f76e78ef45ffeb06fef35558755fe99
Oscar Swanros <swanros@gmail.com>
2015-04-27 18:48:15 -0500

    Add function to create a type-safe Driver instance from a JSON object

--- taximatico_ios/Models/Driver.swift
+++ taximatico_ios/Models/Driver.swift
@@ -17,5 +17,22 @@ struct Driver {
     var id: Int?
     var name: String?
     var taxiId: Int?
-    var location: Location?
+    
+    static func fromJSON(json: [String:AnyObject]) -> Driver? {
+        let mkDriver = curry { id, name, taxiId in Driver(id: id, name: name, taxiId: taxiId) }
+        
+        return toDict(json) >>>= {
+                mkDriver <*> int($0, "id")!
+                        <*> string($0, "name")!
+                        <*> int($0, "taxi_number")!
+        }
+    }
+}
+
+extension Driver: Printable {
+    var description: String {
+        get {
+            return "\nDriver:\n\tId: \(self.id!)\n\tName: \(self.name!)\n\tTaxi ID: \(self.taxiId!)"
+        }
+    }
 }
