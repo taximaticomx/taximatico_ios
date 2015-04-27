96f96df2de0cf3a11a659fed9e83f065e122528d
Oscar Swanros <swanros@gmail.com>
2015-04-27 18:25:31 -0500

    Custom operators and functional functions

--- taximatico_ios/TXFoundation/TXFoundation.swift
+++ taximatico_ios/TXFoundation/TXFoundation.swift
@@ -10,6 +10,8 @@ import Foundation
 
 let userDefaults = NSUserDefaults.standardUserDefaults()
 
+// MARK: - Utilities
+
 func tx_executeOnMainThread(block: (() -> ())?) {
     if let block = block {
         if NSThread.isMainThread() {
@@ -29,7 +31,16 @@ func tx_URLEncode(o: AnyObject) -> String {
     return ""
 }
 
-// MARK: - Get
+
+// MARK: - Functional stuff
+
+func flatten<A>(x: A??) -> A? {
+    if let y = x { return y }
+    return nil
+}
+
+
+// MARK: - User Defaults Get
 
 func user_defaults_get_string(key: String) -> String {
     let s = userDefaults.objectForKey(key) as? String
@@ -49,7 +60,7 @@ func user_defaults_get_integer(key: String) -> Int {
 }
 
 
-// MARK: - Set
+// MARK: - User Defaults Set
 
 func user_defaults_set_string(key: String, val: String?) -> Bool {
     userDefaults.setObject(val, forKey: key)
@@ -64,4 +75,59 @@ func user_defaults_set_bool(key: String, val: Bool) -> Bool {
 func user_defaults_set_integer(key: String, val: Int) -> Bool {
     userDefaults.setInteger(val, forKey: key)
     return userDefaults.synchronize()
+}
+
+
+// MARL: - Casting
+
+func toDict(x: AnyObject) -> [String:AnyObject]? {
+    return x as? [String:AnyObject]
+}
+
+
+// MARK: - Getting Data From [String:AnyObject]s
+
+func number(input: [NSObject:AnyObject], key: String) -> NSNumber? {
+    return input[key] >>>= { $0 as? NSNumber }
+}
+
+func int(input: [NSObject:AnyObject], key: String) -> Int? {
+    return number(input, key).map { $0.integerValue }
+}
+
+func string(input: [String:AnyObject], key: String) -> String? {
+    return input[key] >>>= { $0 as? String }
+}
+
+func bool(input: [String:AnyObject], key: String) -> Bool? {
+    return number(input, key).map { $0.boolValue }
+}
+
+func array(input: [String:AnyObject], key: String) -> [AnyObject]? {
+    let maybeAny: AnyObject? = input[key]
+    return maybeAny >>>= { $0 as? [AnyObject] }
+}
+
+func arrayOf<A>(input: [String:AnyObject], key: String) -> [A]? {
+    let maybeAny: AnyObject? = input[key]
+    return maybeAny >>>= { $0 as? [A] }
+}
+
+func something<A>(input: [String:AnyObject], key: String, type: A) -> A? {
+    return input[key] >>>= { $0 as? A }
+}
+
+
+// MARK: - Currys
+
+func curry<A, B, R> (f: (A, B) -> R) -> A -> B -> R {
+    return { a in { b in f(a, b) } }
+}
+
+func curry<A, B, C, R> (f: (A, B, C) -> R) -> A -> B -> C -> R {
+    return { a in { b in { c in f(a, b, c) } } }
+}
+
+func curry<A, B, C, D, R> (f: (A, B, C, D) -> R) -> A -> B -> C -> D -> R {
+    return { a in { b in { c in { d in f(a, b, c, d) } } } }
 }
