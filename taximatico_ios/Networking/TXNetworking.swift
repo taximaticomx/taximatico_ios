b70f445a250eb17a79840af3a53532a6a2c0bd97
Oscar Swanros <swanros@gmail.com>
2015-04-28 01:19:33 -0500

    Refactor TXNetworking

--- taximatico_ios/Networking/TXNetworking.swift
+++ taximatico_ios/Networking/TXNetworking.swift
@@ -8,6 +8,57 @@
 
 import Foundation
 
+public enum HTTPMethod: String {
+    case OPTIONS    = "OPTIONS"
+    case GET        = "GET"
+    case HEAD       = "HEAD"
+    case POST       = "POST"
+    case PUT        = "PUT"
+    case PATCH      = "PATCH"
+    case DELETE     = "DELETE"
+    case TRACE      = "TRACE"
+    case CONNECT    = "CONNECT"
+}
+
+let URLSession = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration())
+
+func requestWithURL(url: NSURL?, method: HTTPMethod = .GET, customHeaders: [String:String] = ["":""]) -> NSMutableURLRequest? {
+    if let url = url {
+        let request = NSMutableURLRequest(URL: url)
+        
+        request.HTTPMethod = method.rawValue
+        for element in customHeaders {
+            request.setValue(element.1, forHTTPHeaderField: element.0)
+        }
+        
+        return request
+    }
+    
+    return nil
+}
+
+func dataFromJSON(json: AnyObject) -> NSData? {
+    var dataError: NSError?
+    let data = NSJSONSerialization.dataWithJSONObject(json, options: .PrettyPrinted, error: &dataError)
+    
+    if let error = dataError {
+        return nil
+    }
+    
+    return data
+}
+
+func JSONFromData(data: NSData) -> [String:AnyObject]? {
+    var jsonError: NSError?
+    let json = NSJSONSerialization.JSONObjectWithData(data, options: .MutableContainers, error: &jsonError) as? [String:AnyObject]
+    
+    if let json = json where jsonError == nil {
+        return json
+    }
+    
+    return nil
+}
+
 func api_sendRegistrationRequest(phoneNumber ph: String, completionHandler: ((Bool) -> Void)?) {
     TXNetworkActivityIndicator.showNetworkActivityIndicator()
     
@@ -17,32 +68,22 @@ func api_sendRegistrationRequest(phoneNumber ph: String, completionHandler: ((Bo
         ]
     ]
     
-    var jsonError: NSError?
-    let json = NSJSONSerialization.dataWithJSONObject(info, options: .PrettyPrinted, error: &jsonError)
+    let request = requestWithURL(
+        UserDomainEndpoint.Registrations.URL(),
+        method: .POST,
+        customHeaders: ["Content-Type":"application/json"]
+    )
+    request!.HTTPBody = dataFromJSON(info)
     
-    var request = NSMutableURLRequest(URL: UserDomainEndpoint.Registrations.URL())
-    request.HTTPMethod = "POST"
-    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-    request.HTTPBody = json
-    
-    let dataTask = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration()).dataTaskWithRequest(request, completionHandler: { data, response, error in
+    let dataTask = URLSession.dataTaskWithRequest(request!, completionHandler: { data, response, error in
         TXNetworkActivityIndicator.hideNetworkActivityIndicator()
         
-        if let handler = completionHandler where error != nil {
-            dispatch_async(dispatch_get_main_queue()) {
-                handler(false)
+        if error != nil {
+            tx_executeOnMainThread(false, completionHandler)
+        } else {
+            if let json = JSONFromData(data) {
+                tx_executeOnMainThread(string(json, "status") == "ok" ? true : false, completionHandler)
             }
-        }
-        
-        var dictError: NSError?
-        let jsonDict = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &dictError) as? [String:String]
-        
-        if let
-            json    = jsonDict,
-            handler = completionHandler {
-                dispatch_async(dispatch_get_main_queue(), {
-                    handler(json["status"] == "ok" ? true : false)
-                })
         }
     })
     
@@ -52,43 +93,31 @@ func api_sendRegistrationRequest(phoneNumber ph: String, completionHandler: ((Bo
 func api_sendVerificationCode(verificationCode code: String, completionHandler: ((Bool, String?) -> Void)?) {
     TXNetworkActivityIndicator.showNetworkActivityIndicator()
     
-    
     let info = ["verification_code" : [
         "code" : code
         ]
     ]
     
-    var jsonError: NSError?
-    let jsonData = NSJSONSerialization.dataWithJSONObject(info, options: .PrettyPrinted, error: &jsonError)
+    let request = requestWithURL(
+        UserDomainEndpoint.CodeVerification.URL(),
+        method: .POST,
+        customHeaders: ["Content-Type":"application/json"]
+    )
+    request!.HTTPBody = dataFromJSON(info)
     
-    var request = NSMutableURLRequest(URL: UserDomainEndpoint.CodeVerification.URL())
-    request.HTTPMethod = "POST"
-    request.HTTPBody = jsonData
-    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-    
-    let dataTask = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration()).dataTaskWithRequest(request, completionHandler: {data, response, error in
+    let dataTask = URLSession.dataTaskWithRequest(request!, completionHandler: {data, response, error in
         TXNetworkActivityIndicator.hideNetworkActivityIndicator()
         
-        if let handler = completionHandler where error != nil {
-            dispatch_async(dispatch_get_main_queue()) {
-                handler(false, nil)
+        if error != nil {
+            tx_executeOnMainThread(false, nil, completionHandler)
+        } else {
+            if let json    = JSONFromData(data) {
+                tx_executeOnMainThread(string(json, "status") == "ok" ? true : false, string(json, "authentication_token")!, completionHandler)
             }
-        }
-        
-        var dictError: NSError?
-        let jsonDict = NSJSONSerialization.JSONObjectWithData(data, options: .MutableContainers, error: &dictError) as? [String:String]
-        
-        if let
-            json    = jsonDict,
-            handler = completionHandler {
-                dispatch_async(dispatch_get_main_queue(), {
-                    handler(json["status"] == "ok" ? true : false, json["authentication_token"])
-                })
         }
     })
     
     dataTask.resume()
-    
 }
 
 
@@ -96,41 +125,39 @@ func api_getDrivers(completionHandler: (([Driver]?) -> Void)?) {
     TXNetworkActivityIndicator.showNetworkActivityIndicator()
     
     let info = [
-        "authentication_token" : "389c6bd0b49d72245e51a0deba266801", //currentSession.token,
         "latitude" : "19.264987",
         "longitude" : "-103.710863"
     ]
     
-    var request = NSURLRequest(URL: UserDomainEndpoint.Drivers.URL().tx_URLWithParams(info)!)
+    let request = requestWithURL(
+        UserDomainEndpoint.Drivers.URL().tx_URLWithParams(info),
+        method: .GET,
+        customHeaders: ["X-AUTHENTICATION-TOKEN": "389c6bd0b49d72245e51a0deba266801"]
+    )
     
-    let dataTask = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration()).dataTaskWithRequest(request, completionHandler: { data, response, error in
+    let dataTask = URLSession.dataTaskWithRequest(request!, completionHandler: { data, response, error in
         TXNetworkActivityIndicator.hideNetworkActivityIndicator()
         
-        if let errorHandler = completionHandler where error != nil {
-            dispatch_async(dispatch_get_main_queue()) {
-                errorHandler(nil)
-            }
-        }
-        
-        var dictError: NSError?
-        let jsonDict = NSJSONSerialization.JSONObjectWithData(data, options: .MutableContainers, error: &dictError) as? [String:AnyObject]
-        
-        if let
-            status      = jsonDict!["status"] as? String,
-            driversList = jsonDict!["drivers"] as? [[String:AnyObject]],
-            handler     = completionHandler where status == "ok" {
-                var drivers = [Driver]()
-                
-                for driver in driversList {
-                    if let newDriver = Driver.fromJSON(driver) {
-                        drivers.append(newDriver)
+        if error != nil {
+            println(error)
+            tx_executeOnMainThread(nil, completionHandler)
+        } else {
+            let json = JSONFromData(data)
+            
+            if let
+                status      = string(json!, "status"),
+                driversList = array(json!, "drivers") where status == "ok" {
+                    var drivers = [Driver]()
+                    
+                    for driver in driversList {
+                        if let newDriver = Driver.fromJSON(toDict(driver)!) {
+                            drivers.append(newDriver)
+                        }
                     }
-                }
-                
-                handler(drivers)
-        }else{
-            if let errorHandler = completionHandler {
-                errorHandler(nil)
+                    
+                    tx_executeOnMainThread(drivers, completionHandler)
+            }else{
+                tx_executeOnMainThread(nil, completionHandler)
             }
         }
     })
